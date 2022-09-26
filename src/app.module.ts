0b90bc0406f039b2d9827991cf8e0e02c4afdb53
AntonFartovii <anton@rza.by>
2022-09-26 16:29:47 +0300

    fix: seq

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,15 +23,12 @@ import { AuthController } from './auth/auth.controller';
       envFilePath: '.env'
       // envFilePath: `.${process.env.NODE_ENV}.env`
     }),
-
     SequelizeModule.forRoot({
+      database: process.env.DATABASE_URL,
       dialect: 'postgres',
-      host: process.env.POSTGRES_HOST || process.env.DATABASE_URL,
-      port: Number ( process.env.POSTGRES_PORT ),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadModels: true, // создание таблиц на основе моделей
+      host: 'localhost',
+      port: 5432,
+      autoLoadModels: true,
       models:[User, Role, UserRoles, Post]
     }),
     UsersModule,
